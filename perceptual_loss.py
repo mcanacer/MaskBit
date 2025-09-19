4b58cd7dcddcb80ef48ea94101e02aa1bcfc3f1a
Muhammet Can <muhammedacer.mca@gmail.com>
2025-09-19 09:02:42 +0300

    fixed perceptual loss

--- perceptual_loss.py
+++ perceptual_loss.py
@@ -10,12 +10,9 @@ class PerceptualLoss(nn.Module):
     def setup(self):
         self.model = fm.ResNet50(output='logits', pretrained='imagenet')
 
-        self.mean = jnp.array([0.485, 0.456, 0.406])[None, None, None, :]
-        self.std = jnp.array([0.229, 0.224, 0.225])[None, None, None, :]
-
     def __call__(self, inputs, targets):
-        inputs = inputs * self.std + self.mean
-        targets = targets * self.std + self.mean
+        inputs = (inputs + 1.0) / 2.0
+        targets = (targets + 1.0) / 2.0
 
         inputs = jax.image.resize(inputs, (inputs.shape[0], 224, 224, inputs.shape[-1]), method="bilinear")
         targets = jax.image.resize(targets, (targets.shape[0], 224, 224, targets.shape[-1]), method="bilinear")
